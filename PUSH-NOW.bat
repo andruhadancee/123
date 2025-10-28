2f7e0a11ee106ed536ffc9cc30d3a980063b2e8a
andruhadancee <lisagus712@gmail.com>
2025-10-28 07:14:59 +0300

    Fix vercel config

---
+++ PUSH-NOW.bat
@@ -0,0 +1,13 @@
+@echo off
+chcp 65001 >nul
+cd /d "%~dp0"
+
+git add .
+git commit -m "Fix vercel config"
+git push
+
+echo ГОТОВО! Теперь иди в Vercel Settings и поставь:
+echo Build Command: пустое
+echo Output Directory: . (точка)
+pause
+
