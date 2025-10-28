1dc3a9200d7ffda630955f804d1b44ac13ede601
andruhadancee <lisagus712@gmail.com>
2025-10-28 21:25:31 +0300

    Remove all header buttons from admin panel

--- PUSH-NOW.bat
+++ PUSH-NOW.bat
@@ -3,7 +3,7 @@ chcp 65001 >nul
 cd /d "%~dp0"
 
 git add .
-git commit -m "Make nav buttons transparent and light like social buttons"
+git commit -m "Remove all header buttons from admin panel"
 git push
 
 echo ГОТОВО! Теперь иди в Vercel Settings и поставь:
