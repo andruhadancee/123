a50cdbe0c0c5a1c9d119ba7c4586d4aae5bf7d60
andruhadancee <lisagus712@gmail.com>
2025-10-29 03:30:10 +0300

    Make background slightly lighter (0a0a0a)

--- PUSH-NOW.bat
+++ PUSH-NOW.bat
@@ -3,7 +3,7 @@ chcp 65001 >nul
 cd /d "%~dp0"
 
 git add .
-git commit -m "Fix teams page loading order and make background slightly lighter"
+git commit -m "Make background slightly lighter (0a0a0a)"
 git push
 
 echo ГОТОВО! Теперь иди в Vercel Settings и поставь:
