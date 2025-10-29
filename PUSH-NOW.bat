66e6893cda19d2aa995a3d4e78a70bff9eae8908
andruhadancee <lisagus712@gmail.com>
2025-10-29 03:04:22 +0300

    Make timer smaller and less button-like - subtle design

--- PUSH-NOW.bat
+++ PUSH-NOW.bat
@@ -3,7 +3,7 @@ chcp 65001 >nul
 cd /d "%~dp0"
 
 git add .
-git commit -m "Fix timers: reinit on filter + add debug logs"
+git commit -m "Make timer smaller and less button-like - subtle design"
 git push
 
 echo ГОТОВО! Теперь иди в Vercel Settings и поставь:
