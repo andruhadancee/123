07ed05d2b32a98292a37dfa2c98d8e4b740f992a
andruhadancee <lisagus712@gmail.com>
2025-10-29 03:21:06 +0300

    Make discipline icons in filters bigger and more visible

--- PUSH-NOW.bat
+++ PUSH-NOW.bat
@@ -3,7 +3,7 @@ chcp 65001 >nul
 cd /d "%~dp0"
 
 git add .
-git commit -m "Update color palette: use pure black, add more contrast, modern look"
+git commit -m "Make discipline icons in filters bigger and more visible"
 git push
 
 echo ГОТОВО! Теперь иди в Vercel Settings и поставь:
