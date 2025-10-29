2ca77514ae9b853a0de7f7c5af366a80a87eb897
andruhadancee <lisagus712@gmail.com>
2025-10-29 03:36:56 +0300

    Make team cards and tournament sections lighter and more visible

--- PUSH-NOW.bat
+++ PUSH-NOW.bat
@@ -3,7 +3,7 @@ chcp 65001 >nul
 cd /d "%~dp0"
 
 git add .
-git commit -m "Fix teams page: add discipline info and make cards lighter"
+git commit -m "Make team cards and tournament sections lighter and more visible"
 git push
 
 echo ГОТОВО! Теперь иди в Vercel Settings и поставь:
