97613ffe58603d68ccadc609539dec6630a37489
andruhadancee <lisagus712@gmail.com>
2025-10-29 04:17:45 +0300

    Improve loader screen with gradient background and animated effects

--- PUSH-NOW.bat
+++ PUSH-NOW.bat
@@ -3,7 +3,7 @@ chcp 65001 >nul
 cd /d "%~dp0"
 
 git add .
-git commit -m "Simplify card animation - smooth slide up, shorter delays"
+git commit -m "Improve loader screen with gradient background and animated effects"
 git push
 
 echo ГОТОВО! Теперь иди в Vercel Settings и поставь:
